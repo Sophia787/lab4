3deb6c81c0834397f9aacccec6884a4de222daef
Sophia787 <91783576+Sophia787@users.noreply.github.com>
2021-11-06 17:43:17 +0300

    Update monotonic.py

--- monotonic.py
+++ monotonic.py
@@ -1,16 +1,1 @@
-# Проверяет монотонность в порядке возрастания
-def is_monotonic(nums):
-    for i in range(len(nums)-1):
-        if nums[i] > nums[i+1]:
-            return False
-    return True
 
-
-while True:
-    print("Вход (введите значения списка через пробел): ")
-    nums = [int(i) for i in input('nums = ').split()]
-    #Если введенный список монотонный или введенный перевернутый список монотонный, то True 
-    if is_monotonic(nums) or is_monotonic(list(reversed(nums))):
-        print("Вывод: True")
-    else:
-        print("Вывод: False")
